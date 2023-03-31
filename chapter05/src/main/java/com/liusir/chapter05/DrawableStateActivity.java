40d7ae92e5a3e4310504f80b0d357a01a6ad73ff
unswerving_blue <liusir0130@163.com>
2023-03-31 16:49:08 +0800

    装态列表图形

--- chapter05/src/main/java/com/liusir/chapter05/DrawableStateActivity.java
+++ chapter05/src/main/java/com/liusir/chapter05/DrawableStateActivity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DrawableNineActivity extends AppCompatActivity {
+public class DrawableStateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawable_nine);
-
+        setContentView(R.layout.activity_drawable_state);
     }
 }
