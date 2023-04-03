4482308227d81a262ccbcef7e017f3a0245125bb
unswerving_blue <liusir0130@163.com>
2023-04-03 18:51:53 +0800

    开关按钮swith

--- chapter05/src/main/java/com/liusir/chapter05/switch_acitvity.java
+++ chapter05/src/main/java/com/liusir/chapter05/switch_acitvity.java
@@ -3,12 +3,31 @@ package com.liusir.chapter05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class switch_acitvity extends AppCompatActivity {
+public class switch_acitvity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_switch_acitvity);
+//        原生switch
+        Switch swichOriginal = (Switch) findViewById(R.id.sw_status);
+        swichOriginal.setOnCheckedChangeListener(this);
+//        自定义swich
+        CheckBox switchSelf = (CheckBox) findViewById(R.id.cb_status);
+        switchSelf.setOnCheckedChangeListener(this);
+        tvResult = (TextView) findViewById(R.id.tv_result);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        String format = String.format("Switch的状态是%s", isChecked ? "开" : "关");
+        tvResult.setText(format);
     }
 }
