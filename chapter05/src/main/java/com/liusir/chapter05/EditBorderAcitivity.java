7467098c12f00aefcd8957d14f31e5c425d813c7
unswerving_blue <liusir0130@163.com>
2023-04-06 18:23:48 +0800

    给EditText设置自定义样式

---
+++ chapter05/src/main/java/com/liusir/chapter05/EditBorderAcitivity.java
@@ -0,0 +1,14 @@
+package com.liusir.chapter05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditBorderAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_border_acitivity);
+    }
+}
